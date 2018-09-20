fb3cbbc7cbb6a9a25f2045d6e105b1511a634558
Andrew Jarrett <ahrjarrett@gmail.com>
2018-09-20 12:41:19 -0500

    correctly logging urls from html file; need to tweak regex still

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -14,19 +14,17 @@ const filename = args[0]
 
 const regex = /http[s]?:\/\/[\w.]+(.com|.net)/g
 
-fs.readFile(filename, 'utf-8', (err, data) => {
+fs.readFile(filename, 'utf-8', (err, data) => console.log(data.match(regex)))
 
-})
-
-console.log('https://google.com',       '\n', regex.test('https://google.com'))
-console.log('http://google.com',        '\n', regex.test('http://google.com'))
-console.log('https://www.google.com',   '\n', regex.test('https://www.google.com'))
-console.log('http://www.google.com',    '\n', regex.test('http://www.google.com'))
-console.log('https://www.google.net',   '\n', regex.test('https://www.google.net'))
-console.log('https://www.google',       '\n', regex.test('https://www.google'))
-console.log('https://www.google',       '\n', regex.test('https://www.google'))
-
-console.log('https://this.that.google.com',       '\n', regex.test('https://this.that.google.com'))
+//console.log('https://google.com',       '\n', regex.test('https://google.com'))
+//console.log('http://google.com',        '\n', regex.test('http://google.com'))
+//console.log('https://www.google.com',   '\n', regex.test('https://www.google.com'))
+//console.log('http://www.google.com',    '\n', regex.test('http://www.google.com'))
+//console.log('https://www.google.net',   '\n', regex.test('https://www.google.net'))
+//console.log('https://www.google',       '\n', regex.test('https://www.google'))
+//console.log('https://www.google',       '\n', regex.test('https://www.google'))
+//
+//console.log('https://this.that.google.com',       '\n', regex.test('https://this.that.google.com'))
 
 // Set up regex
 
