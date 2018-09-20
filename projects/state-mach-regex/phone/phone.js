a547b6bb2f61abaef78159af3c67547531673a47
unknown <alexisreyes90@gmail.com>
2018-09-19 22:25:11 -0400

    phone number regex complete?

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -6,22 +6,24 @@ var rl = readline.createInterface({
   terminal: false
 });
 
-// phone number could be (123)-123-1234, 123-123-1234, 123 123 1234, 123-123, 1234, etc etc
-
-const regexNum = ^\(?d{3}\)?[\s,*-.]?d{3}[\s,*-.]?d{4}? // this does not account for a +1 in front of the phone number
-const regexNum2 = ^\+?\d{0-3}\s?\(?d{3}\)?[\s,*-.]?d{3}[\s,*-.]?d{4}?
-
 // This code reads a line at a time from stdin
 
 rl.on('line', function (line) {
 
-    // !!!! IMPLEMENT ME
+  console.log(line);
 
     // Come up with the phone regex
+    // phone number could be (123)-123-1234, 123-123-1234, 123 123 1234, 123-123, 1234, etc etc
+    const regexNum = /\(?\d{3})?\s?\(?\d{3}\)?[\s,*-.]?\d{3}[\s,*-.]?\d{4}/;
 
     // Find matches
+    const number = line.match(regexNum);
 
     // If match found, print number with no spaces, parens, or dashes
+    if(number === Null) {
+      console.log("Nothing!");
 
-    // Else print that no number was found
+    } else {
+      console.log(`Found the number! ${number}`);
+    }
 });
