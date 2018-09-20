c59c541de4bf10f1343bfe745bb20199fb4b7ebc
unknown <alexisreyes90@gmail.com>
2018-09-19 23:36:59 -0400

    WIP extractlinks

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -9,15 +9,17 @@ if (args.length != 1) {
 
 const filename = args[0];
 
+const data = fs.readFileSync(filename,{encoding: 'utf8' });
+
 // !!!! IMPLEMENT ME
 const regexURL = /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?/;
 
 // Read file
-const website = filename.match(regexURL);
+const website = data.match(regexURL);
 // Set up regex
 
 // Find matches
-for ( sites in filename) {
-    console.log(filename[sites]);
+for ( sites in website) {
+    console.log(sites);
 }
 // Print all matches
