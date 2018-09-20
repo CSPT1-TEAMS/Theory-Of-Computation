ce843a793f7d0f38ed04fb6653d4b2c583673b35
Andrew Jarrett <ahrjarrett@gmail.com>
2018-09-20 12:12:30 -0500

    readfile for stackoverflow regex

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -1,17 +1,20 @@
-const fs = require('fs');
+const fs = require('fs')
 
-const args = process.argv.slice(2);
+const readfile = path =>
+      fs.readFile(path, 'utf-8', (err, data) => data)
+
+const args = process.argv.slice(2)
 
 if (args.length != 1) {
-    console.error("usage: extractlinks inputfile");
-    process.exit(1);
+    console.error("usage: extractlinks inputfile")
+    process.exit(1)
 }
 
-const filename = args[0];
+const filename = args[0]
 
-// !!!! IMPLEMENT ME
+let text = fs.readFile(filename, 'utf-8', (err, data) => data)
 
-// Read file
+console.log(text)
 
 // Set up regex
 
