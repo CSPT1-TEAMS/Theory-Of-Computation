9694096727be92d5353e789a26b9858e020ee252
unknown <alexisreyes90@gmail.com>
2018-09-19 23:39:35 -0400

    We just added global, now all keys match extractlink complete

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -9,10 +9,10 @@ if (args.length != 1) {
 
 const filename = args[0];
 
-const data = fs.readFileSync(filename,{encoding: 'utf8' });
+const data = fs.readFileSync(filename, {encoding: 'utf8' });
 
 // !!!! IMPLEMENT ME
-const regexURL = /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?/;
+const regexURL = /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?/g;
 
 // Read file
 const website = data.match(regexURL);
