eec40a02e72fbe74d34195d26d3f919eadb08002
Andrew Jarrett <ahrjarrett@gmail.com>
2018-09-20 21:37:23 -0500

    configure readline to print back area, prefix and suffix on basic regex phone match

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -1,23 +1,19 @@
-var readline = require('readline');
+let readline = require('readline')
 
-var rl = readline.createInterface({
+let rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   terminal: false
-});
+})
 
-
-// This code reads a line at a time from stdin
+let regex = /^(\d{3})(\d{3})(\d{4})$/
 
 rl.on('line', function (line) {
+  console.log(regex.test(line))
+  console.log('match:', matches)
 
-    // !!!! IMPLEMENT ME
-
-    // Come up with the phone regex
-
-    // Find matches
-
-    // If match found, print number with no spaces, parens, or dashes
-
-    // Else print that no number was found
-});
+  matches = regex.exec(line).slice(1,4)
+  console.log('Area code:', matches[0])
+  console.log('Prefix:', matches[1])
+  console.log('Suffix:', matches[2])
+})
