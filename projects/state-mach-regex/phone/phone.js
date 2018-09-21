f57e6912cc391a3c2296900b6d2fb678161486e5
Andrew Jarrett <ahrjarrett@gmail.com>
2018-09-20 21:59:02 -0500

    complete theory of computation sprint

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -6,19 +6,20 @@ let rl = readline.createInterface({
   terminal: false
 })
 
-let regex = /^(\(\d{3}\)|\d{3})[\s-]*(\d{3})[\s-]*(\d{4})$/
+let regex = /^(\(\d{3}\)\s?|\d{3})[\s-]?(\d{3})[\s-]?(\d{4})$/
 
-console.log('Enter a phone number:')
+console.log(`\n    Enter a phone number:`)
 rl.on('line', function (line) {
-  if(regex.test(line)) {
-    let matches = regex.exec(line).slice(1,4)
-    console.log('match:', matches)
-    console.log('Area code:', matches[0])
-    console.log('Prefix:', matches[1])
-    console.log('Suffix:', matches[2])
-  }
-  else {
-    console.log('Invalid phone number. Try again.')
-  }
+  if(!regex.test(line))
+    return console.log('Invalid phone number. Try again.')
+
+  let matches = regex.exec(line).slice(1,4)
+
+  console.log(`
+Area code :: ${matches[0]}
+   Prefix :: ${matches[1]}
+   Suffix :: ${matches[2]}
+
+Enter another phone number:`)
 })
 
