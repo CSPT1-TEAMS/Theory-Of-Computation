52a0e639179637dc2399f0518466a7ca51ccd4d5
Stephanie Bowles <sbowl001@fiu.edu>
2018-09-24 00:32:08 +0800

    init

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -9,6 +9,10 @@ if (args.length != 1) {
 
 const filename = args[0];
 
+
+const filedata = fs.readFileSync(filename, {encoding: 'utf8'});
+//confused as to why we do this
+
 // !!!! IMPLEMENT ME
 
 // Read file
@@ -17,4 +21,10 @@ const filename = args[0];
 
 // Find matches
 
+const matches = filedata.match(regex);
+
 // Print all matches
+
+for (let url of matches) {
+    console.log(url);
+}
